c17da29f4893feaa1ed6324355358bd7ded87fac
Kleber Sobrinho <kleber.sobrinho@ccc.ufcg.edu.br>
2021-05-11 16:59:37 -0300

    Update Components

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -4,25 +4,27 @@ import {TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Left, Info, Name, Priority} from './styles';
+import {Container, Left, Right, Info, Name, Priority} from './styles';
 
 export default function Task({data, onCancel, onEdit}) {
   return (
     <Container>
       <Left>
         <Info>
-          <Name>Nome: {data.name}</Name>
-          <Priority>Prioridade: {data.priority}</Priority>
+          <Name>{data.name}</Name>
+          <Priority>{data.priority}</Priority>
         </Info>
       </Left>
 
-      <TouchableOpacity onPress={onCancel}>
-        <Icon name="delete" size={20} color="#f64c" />
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={onEdit}>
-        <Icon name="create" size={20} color="#f6" />
-      </TouchableOpacity>
+      <Right>
+        <TouchableOpacity onPress={onEdit}>
+          <Icon name="create" size={20} color="#3F3D56" />
+        </TouchableOpacity>
+        
+        <TouchableOpacity onPress={onCancel}>
+          <Icon name="delete" size={20} color="#3F3D56" />
+        </TouchableOpacity>
+      </Right>
     </Container>
   );
 }
